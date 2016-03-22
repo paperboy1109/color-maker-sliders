c27f24d0edb4bc2eccbbf9853f1e9ab78c2cadf4
Daniel J Janiak <danieljjaniak@Daniels-MacBook-Air.local>
2016-03-22 09:00:54 -0600

    Use rgb sliders to adjust color

--- color-maker-sliders/ViewController.swift
+++ color-maker-sliders/ViewController.swift
@@ -10,14 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    @IBOutlet var colorView: UIView!
+    
+
+    
+    @IBAction func adjustColor(Sender: AnyObject) {
+        
+        // Make sure the program doesn't crash if the controls aren't connected
+        if self.redSlider == nil {
+            return
+        }
+        
+        let r: CGFloat = CGFloat(self.redSlider.value)
+        let g: CGFloat = CGFloat(self.greenSlider.value)
+        let b: CGFloat = CGFloat(self.blueSlider.value)
+        
+        colorView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
 
